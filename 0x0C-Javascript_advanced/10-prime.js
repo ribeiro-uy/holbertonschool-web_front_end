f998a082b79fd50bdaa09267d803c895746b192e
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-09-22 16:55:29 -0300

    task 10

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,24 @@
+let start = performance.now()
+function countPrimeNumbers() {
+    counter = 0;
+    for (let i = 2; i <= 100; i++) {
+        if (i == 2) {
+            counter++;
+        }
+        for (let n = 2; n < i; n++) {
+            if (i % n == 0) {
+                break
+            }
+            else if (n + 1 < i) { }
+            else {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+for (let j = 0; j < 100; j++) {
+    result = countPrimeNumbers();
+}
+let end = performance.now()
+console.log('Execution time of calculating prime numbers 100 times was ' + (end - start) + ' milliseconds.');
