2e256dff165ed6d64de34f88906be7fa5c865f16
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-09-22 09:46:08 -0300

    task 6

---
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,38 @@
+class studentHogwarts {
+    privateScore = 0;
+    #name = null;
+
+    #changeScoreBy(points) {
+        this.privateScore += points;
+    }
+    setName(newName){
+        this.#name = newName;
+    }
+    rewardStudent(){
+        this.#changeScoreBy(1);
+    }
+    penalizeStudent(){
+        this.#changeScoreBy(-1);
+    }
+    getScore(){
+        return this.#name + ": " + this.privateScore;
+    }
+}
+
+exports = { studentHogwarts }
+
+harry = new studentHogwarts;
+harry.setName("Harry");
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+draco = new studentHogwarts;
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent()
+draco.penalizeStudent();
+console.log(draco.getScore());
