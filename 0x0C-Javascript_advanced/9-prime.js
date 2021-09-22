805d30fd0e0c265021486afe4df692aeb9238e2f
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-09-22 16:44:37 -0300

    task 9

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,22 @@
+var start = performance.now()
+function countPrimeNumbers() {
+    counter = 0;
+    for (let i = 2; i <= 100; i++) {
+        if (i == 2) {
+            counter++;
+        }
+        for (let n = 2; n < i; n++) {
+            if (i % n == 0) {
+                break
+            }
+            else if (n + 1 < i) { }
+            else {
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+result = countPrimeNumbers();
+var end = performance.now();
+console.log('Execution time of printing countPrimeNumbers was ' + (end - start) + ' milliseconds.');
