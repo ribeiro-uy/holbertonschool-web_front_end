126eedcd1273ec58cff04a35984aa5d350ce6e83
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-09-22 16:55:48 -0300

    task 12

---
+++ 0x0C-Javascript_advanced/12-room_area.js
@@ -0,0 +1,10 @@
+let roomDimensions = {
+    width: 50,
+    length: 100,
+    getArea: function () {
+        let surface = this.width * this.length;
+        return surface;
+    }
+
+};
+let boundGetArea = roomDimensions.getArea.bind(roomDimensions);
