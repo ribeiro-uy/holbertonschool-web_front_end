7e200658a74c6cff57d59f2ebb6d256867b2331a
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-09-22 09:45:43 -0300

    task 3

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,14 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        function result(){
+            return seat;
+        }
+        return result;
+    }
+    let students = [];
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+const classRoom = createClassRoom(10);
