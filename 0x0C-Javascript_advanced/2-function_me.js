034f8c882832f0df22f0c6fd446de68017c872aa
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-09-22 09:45:36 -0300

    task 2

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,6 @@
+function welcomeMessage(fullname) {
+    return alert("Welcome " + fullname);
+}
+var guillaume = () => welcomeMessage("Guillaume");
+var alex = () => welcomeMessage("Alex");
+var fred = () => welcomeMessage("Fred");
